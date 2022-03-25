dcea5c2a8f2ad8ec0db7b095ce108b6484d6dbde
Orlando <charlyyuman@gmial.com>
2022-03-25 16:51:14 -0600

    programa que convierte  numeros arabigos a Romanos

---
+++ Arabigo-Romano_convert/src/main/java/com/mycompany/arabigo/romano_convert/ArabigoRomano_convert.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.arabigo.romano_convert;
+
+/**
+ *
+ * @author selvi
+ */
+public class ArabigoRomano_convert {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        principal nVentana= new principal();
+        nVentana.show();
+    }
+}
